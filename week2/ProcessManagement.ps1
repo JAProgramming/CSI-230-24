607ef51d65f857dcdc9416187156d99c2e4c90ec
Justin <justin.allman@mymail.champlain.edu>
2024-10-01 22:24:59 -0400

    Proccess Management Lab

---
+++ week2/ProcessManagement.ps1
@@ -0,0 +1,20 @@
+﻿#Get-Process | Where-Object { $_.ProcessName -match '^C'} | Select-Object ProcessName
+
+#Get-Process | Where-Object {$_.Path -notlike "*system32*"} | Select-Object name, path
+
+#$Servicestop = Get-Service | Where-Object {$_.Status -eq "Stopped"}
+
+#$Sortserv = $Servicestop | Sort-Object -Property DisplayName
+
+#$Sortserv | Export-Csv -path "C:\stopped_services.csv" -Notypeinformation
+
+$Namechrome = Get-Process | Where-Object { $_.Processname -eq 'Chrome'}
+
+if($Namechrome -eq $null){
+    
+    start-process "C:\Program Files\Google\Chrome\application\chrome.exe" "www.champlain.edu" 
+
+} else {
+
+    Stop-Process -name Chrome
+}
