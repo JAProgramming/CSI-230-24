b4a5acc99b80c27ab37b2bb82472caa911602b5e
Justin <justin.allman@mymail.champlain.edu>
2024-10-03 15:12:13 -0400

    Calls the functions from Function and Event Logs

---
+++ week2/FunctionCalls.ps1
@@ -0,0 +1,14 @@
+﻿. (join-path $PSScriptRoot Functions and Event Logs.ps1)
+
+clear
+
+$loginouttable = newFunction(-90)
+$loginouttable
+
+$systemon = startup(-90)
+$systemon
+
+$shutdown = shutdowns(-90)
+$shutdown
+
+$newvar = $($loginouttable; $systemon; $shutdown)
