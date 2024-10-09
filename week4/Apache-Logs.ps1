625576e035423739ab5dd9e66f689bdfc7fbfe92
Justin <justin.allman@mymail.champlain.edu>
2024-10-09 14:06:32 -0400

    Week 4 Windows apache logs

---
+++ week4/Apache-Logs.ps1
@@ -0,0 +1,8 @@
+﻿function apachelogs( $page, $code, $browser){
+    $returns = Get-Content C:\xampp\apache\logs\access.log | Select-String "/$page " | Select-String " $code " | Select-String " $browser/"
+
+    return $returns
+} 
+
+$returns = apachelogs "*" "404" "chrome"
+$returns
---
+++ week4/WindowsApacheLogs.ps1
@@ -0,0 +1,29 @@
+﻿#Get-Content C:\xampp\apache\logs\access.log
+
+#Get-Content C:\xampp\apache\logs\access.log -Tail 5
+
+#Get-Content C:\xampp\apache\logs\access.log | Select-String -Pattern ' 404 ', ' 400 '
+
+#Get-Content C:\xampp\apache\logs\access.log | Select-String ' 200 ' -NotMatch
+
+#$A = Get-ChildItem C:\xampp\apache\logs\*.log | Select-String -Pattern 'error'
+#$A[-5..-1]
+
+$NotFound = Get-Content C:\xampp\apache\logs\access.log | Select-String -Pattern ' 404 '
+
+$regex = [regex] "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+
+$ipunorganized = $regex.Matches($NotFound)
+
+$ips = @()
+
+for($i=0; $i -lt $ipunorganized.count; $i++) {
+$ips += [pscustomobject]@{ "IP" = $ipunorganized[$i].value;}
+}
+#$ips | Where-Object { $_.IP -ilike "10.*"}
+
+
+
+$ipsoftens = $ips | Where-Object { $_.IP -ilike "10.*" }
+$counts = $ipsoftens | group IP
+$counts | select-object Count, Name
