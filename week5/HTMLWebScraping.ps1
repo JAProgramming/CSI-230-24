d75d58070c939e24eddd95ad3087faa6d7b18702
Justin <justin.allman@mymail.champlain.edu>
2024-10-13 20:09:57 -0400

    Week five folder

---
+++ week5/HTMLWebScraping.ps1
@@ -0,0 +1,14 @@
+﻿$scraped_page = Invoke-WebRequest -TimeoutSec 10 http://10.0.17.42/ToBeScraped.html
+#$scraped_page.links.count
+
+#$scraped_page.links
+
+#$scraped_page.Links | Select-Object outerText, href
+
+#$hs1=$scraped_page.Parsedhtml.body.getElementsByTagName("h2") | select outerText
+#$hs1
+
+$divs1=$scraped_page.Parsedhtml.body.getElementsByTagName("div") | where {
+$_.getAttributeNode("class").Value -ilike "*div-1"} | select innerText
+$divs1
+
