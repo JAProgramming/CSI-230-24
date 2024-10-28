df2a5ad150e6bed1da0c1df95e9bcff1fafbe732
Justin <justin.allman@mymail.champlain.edu>
2024-10-27 22:39:13 -0400

    Everything is done, just could not understand the at risk function so I did not do that part

---
+++ week6/String-Helper.ps1
@@ -0,0 +1,58 @@
+﻿<# String-Helper
+*************************************************************
+   This script contains functions that help with String/Match/Search
+   operations. 
+************************************************************* 
+#>
+
+
+<# ******************************************************
+   Functions: Get Matching Lines
+   Input:   1) Text with multiple lines  
+            2) Keyword
+   Output:  1) Array of lines that contain the keyword
+********************************************************* #>
+function getMatchingLines($contents, $lookline){
+
+$allines = @()
+$splitted =  $contents.split([Environment]::NewLine)
+
+for($j=0; $j -lt $splitted.Count; $j++){  
+ 
+   if($splitted[$j].Length -gt 0){  
+        if($splitted[$j] -ilike $lookline){ $allines += $splitted[$j] }
+   }
+
+}
+
+return $allines
+}
+
+<# ***************************************************************************
+Check Password
+****************************************************************************** #>
+
+function checkPassword($password){
+
+    write-host $passwd
+    if ($passwd.length  -lt 6){
+        Write-Host "Failed Length Test" | Out-String
+        return $False
+        }
+    elseif($passwd -notlike "*[0-9]*"){
+        Write-Host "Digit Test" | Out-String
+        return $False
+        }
+    elseif($passwd -notlike "*[!a-zA-Z0-9]*"){
+        Write-Host "Failed Special C Test" | Out-String
+        return $False
+        }
+    elseif($passwd -notlike "*[a-zA-Z]*"){
+        Write-Host "Failed Letter Test" | Out-String
+        return $False
+        }
+
+    else{
+    Write-Host "Here"
+    return $true }
+}
